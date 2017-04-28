23e6b66c1096588cf3e820962d2ab83a0bf535fb
Daniel <danaguilar6@gmail.com>
2017-04-28 12:00:29 -0500

    Created win condition and reset button

--- mastermind.rb
+++ mastermind.rb
@@ -2,27 +2,42 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './libs/mastermind.rb'
 
-
-
-def make_new_code(code)
-  @game.setup_original(code)
-end
-
-def init_game
-  @game = Mastermind.new(['1','2','3','4'],4,12)
-  make_new_code(['1','2','3','4'])
+@@game = Mastermind.new(['1','2','3','4'],4,12)
+@@turn = 1
+def make_new_code
+  code = Array.new
+  options = ['1','2','3','4']
+  4.times do
+    code << options.sample
+  end
+  @@game.setup_original(code)
 end
 
 def make_guess(guess)
-  @game.make_guess(guess)
-  @game.check_guess
+  @@game.make_guess(guess)
+  @@game.check_guess
 end
 
+def guess(params)
+  guess = Array.new
+  guess << params["input1"]
+  guess << params["input2"]
+  guess << params["input3"]
+  guess << params["input4"]
+  make_guess(guess.join(','))
+end
+
+make_new_code
 get '/' do
-  init_game unless @game
-  make_guess("1,1,1,1")
-  make_guess("2,2,2,2")
-  make_guess("3,4,1,1")
-  checked_guess = @game.board.compare_codes
-  erb :index, :locals => {:hidden_code => @game.board.original, :old_guesses => @game.old_guesses,:old_responses => @game.old_responses}
+  if params['reset']
+    @@game = Mastermind.new(['1','2','3','4'],4,12)
+    @@turn = 1
+    make_new_code
+  elsif params["input1"]
+    guess(params)
+    @@turn += 1
+  end
+  checked_guess = @@game.board.compare_codes
+  erb :index, :locals => {:hidden_code => @@game.board.original, :old_guesses => @@game.old_guesses,:old_responses => @@game.old_responses, :turn => @@turn,
+  :won => (@@game.has_won?)}
 end
